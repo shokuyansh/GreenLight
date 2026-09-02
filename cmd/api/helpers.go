0c4e4bc2bcccd4b4fe3e071a4c0bd6845c63c64b
shokuyansh <anshmalgotra@gmail.com>
2026-09-02 12:25:19 +0530

    Initialized the error and made 2 handlers(pg 1-45)

---
+++ cmd/api/helpers.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+	"strconv"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func (app *application) readIDParam(r *http.Request) (int, error) {
+	params := httprouter.ParamsFromContext(r.Context())
+
+	id, err := strconv.Atoi(params.ByName("id"))
+	if err != nil || id < 1 {
+		return 0, errors.New("invalid id parameter")
+	}
+	return id, nil
+}
+
+func (app *application) writeJSON(w http.ResponseWriter, data any, statusCode int, headers http.Header) error {
+	js, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	js = append(js, '\n')
+
+	for key, value := range headers {
+		w.Header()[key] = value
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+	w.Write(js)
+
+	return nil
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/shokuyansh/GreenLight
+
+go 1.26.5
+
+require github.com/julienschmidt/httprouter v1.3.0 // indirect
